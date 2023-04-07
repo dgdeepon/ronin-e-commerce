ec4e97025e34eb34eb38858ecd8c4ddcefb46079
dgdeepon <deeponghosh890@gmail.com>
2023-04-07 20:18:32 +0530

    firebase setup

--- ronin/src/App.js
+++ ronin/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Products from './components/Products';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Products/>
     </div>
   );
 }
