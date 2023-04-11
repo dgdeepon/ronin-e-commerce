26b453f8e37d6006e7f25e1b8f594d1b467ae154
suraj Yadav <111379728+suraj826580@users.noreply.github.com>
2023-04-11 20:27:15 +0530

    Carousel

---
+++ ronin/src/components/Carouel/HeaderCarousel.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Box, IconButton, Image, useBreakpointValue } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleChevronLeft,
+  faCircleChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const imageArray = [
+  {
+    image:
+      "https://images.bewakoof.com/uploads/grid/app/college-banner-common-desktop-1681194853.jpg",
+  },
+  {
+    image:
+      "https://images.bewakoof.com/uploads/grid/app/1x1-fresh-summer-arrivals-common.jpg",
+  },
+  {
+    image:
+      "https://images.bewakoof.com/uploads/grid/app/1x1-joggers-common-w-bdayunit-1680263895.jpg",
+  },
+];
+export default function HeaderCarousel() {
+  const top = useBreakpointValue({ base: "90%", md: "40%" });
+  const side = useBreakpointValue({ base: "30%", md: "40px" });
+  const [slider, setSlider] = React.useState(imageArray);
+
+  var settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+  };
+  return (
+    <Box overflow={"hidden"}>
+      <IconButton
+        _hover={{
+          background: "tranparent",
+        }}
+        bg="transparent"
+        color={"#fff"}
+        aria-label="left-arrow"
+        position="absolute"
+        left={side}
+        top={top}
+        transform={"translate(0%, -50%)"}
+        zIndex={2}
+        onClick={() => slider.slickPrev()}>
+        <FontAwesomeIcon icon={faCircleChevronLeft} fontSize={"40px"} />
+      </IconButton>
+      {/* Right Icon */}
+      <IconButton
+        _hover={{
+          background: "tranparent",
+        }}
+        bg="transparent"
+        color={"#fff"}
+        aria-label="right-arrow"
+        variant="ghost"
+        position="absolute"
+        right={side}
+        top={top}
+        transform={"translate(0%, -50%)"}
+        zIndex={2}
+        onClick={() => slider.slickNext()}>
+        <FontAwesomeIcon icon={faCircleChevronRight} fontSize={"40px"} />
+      </IconButton>
+      <Slider {...settings}>
+        {imageArray.length &&
+          imageArray.map(({ image }) => {
+            return (
+              <Box key={Math.random()} px={3}>
+                <Image src={image} />
+              </Box>
+            );
+          })}
+      </Slider>
+      <Box mt={3.5}>
+        <Image
+          src="https://images.bewakoof.com/uploads/grid/app/thin-strip-non-tribe-desktop-1681214905.jpg"
+          width={"100%"}
+          objectFit={"cover"}
+        />
+      </Box>
+    </Box>
+  );
+}
--- ronin/src/components/Products.jsx
+++ ronin/src/components/Products.jsx
@@ -1,27 +1,25 @@
-import { Container } from '@chakra-ui/react';
-import axios from 'axios'
-import React from 'react'
-import {db} from '../firebase/firebase';
-import { collection,getDocs, onSnapshot, addDoc} from 'firebase/firestore';
-import { getDatabase, ref, child, get,set } from "firebase/database";
+import { Container } from "@chakra-ui/react";
+import axios from "axios";
+import React from "react";
+import { db } from "../firebase/firebase";
+import { collection, getDocs, onSnapshot, addDoc } from "firebase/firestore";
+import { getDatabase, ref, child, get, set } from "firebase/database";
 
 export default function Products() {
+  async function main() {
+    console.log(db);
+    const id = 1; //I have to make unique id's every time
+    get(ref(db)).then((res) => {
+      console.log(res.val());
+    });
+    set(ref(db, `products/${id}/`), {
+      username: "n",
+      email: "email",
+      profile_picture: "imageUrl",
+    });
+  }
 
-    async function main(){
-        console.log(db);
-        const id=1; //I have to make unique id's every time 
-        get(ref(db)).then((res)=>{
-            console.log(res.val());
-        })
-        set(ref(db, `products/${id}/`), {
-            username: 'n',
-            email: 'email',
-            profile_picture : 'imageUrl'
-          });
-        
-    }
+  // main();
 
-    // main();
-    
-  return 0
+  return "";
 }
