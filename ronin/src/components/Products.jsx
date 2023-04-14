2ad8d710d3e26f51d79cae75ba5f9d09e36143f9
dgdeepon <deeponghosh890@gmail.com>
2023-04-15 01:02:22 +0530

    some features of products page

--- ronin/src/components/Products.jsx
+++ ronin/src/components/Products.jsx
@@ -1,9 +1,8 @@
-import { Container } from "@chakra-ui/react";
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Container, Grid, VStack } from "@chakra-ui/react";
 import axios from "axios";
 import React from "react";
 import { db } from "../firebase/firebase";
-import { collection, getDocs, onSnapshot, addDoc } from "firebase/firestore";
-import { getDatabase, ref, child, get, set } from "firebase/database";
+import { ref, get, set } from "firebase/database";
 
 export default function Products() {
   async function main() {
@@ -12,14 +11,49 @@ export default function Products() {
     get(ref(db)).then((res) => {
       console.log(res.val());
     });
-    set(ref(db, `products/${id}/`), {
-      username: "n",
-      email: "email",
-      profile_picture: "imageUrl",
-    });
   }
 
   // main();
 
-  return "";
+  return <Container>
+    <Grid gridTemplateColumns={'repeat(2,48%)'}>
+        <VStack>
+        <Accordion defaultIndex={[0]} allowMultiple>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex='1' textAlign='left'>
+                  Section 1 title
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex='1' textAlign='left'>
+                  Section 2 title
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+        </VStack>
+    </Grid>
+  </Container>
 }
