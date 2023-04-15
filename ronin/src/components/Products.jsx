934b2a816689986ff38c08f0f3abdc5368e37632
dgdeepon <deeponghosh890@gmail.com>
2023-04-15 19:27:42 +0530

    filteroptions

--- ronin/src/components/Products.jsx
+++ ronin/src/components/Products.jsx
@@ -1,33 +1,56 @@
 import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, Container, Grid, Image, Spinner, Text, VStack } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsNow } from "../redux/ProductsStore/productsAction";
 
 export default function Products() {
-  const store=useSelector((store)=>{
-    return store.productsReducer.data;
-  })
-  const dispatch=useDispatch();
-
-  // console.log(store);
+  const filterData=[
+    {title:'Gender',options:['Men','Unisex']},
+    {title:'Sizes',options:['M','L','XL']},
+    {title:'Brand',options:['Bewakoof®','Rigo','Adro','Dillinger','Mad Over Print','Door 74','Difference Of Opinion','Threadcurry']}
+    ]
+    
+    const store=useSelector((store)=>{
+      return (store.productsReducer.data);
+    })
+    const dispatch=useDispatch();
   
   // products card
   function productsCard(id,image,name,description,price,rating){
-    return <Box key={id}>
-      <Image src={`${image}`} alt={name}/>
+    return <Box key={id} data-id={id}>
+      <Image _hover={{transform:'scale(1.01)'}} src={`${image}`} alt={name}/>
       <Text as='b'>{name}</Text>
-      <Text noOfLines={2}>{description}</Text>
-      <Text as={'b'}>{price}</Text>
-      {rating>0? <Text>{rating}</Text>:''}
+      <Text noOfLines={1}>{description}</Text>
+      <Text as={'b'}>₹{price}</Text>
+      {rating>0? <Text>{rating}</Text>:'\n'}
       <Button data-id={id}>ADD</Button>
     </Box>
   }
 
+  // filters
+  function filters(title,options){
+    return <AccordionItem>
+      <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+          {title}
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      {options.map((el)=>{
+        return <Text>{el}</Text>
+      })}
+    </AccordionPanel>
+      </AccordionItem>
+  }
 
 
   useEffect(()=>{
     dispatch(getProductsNow);
   },[])
+
 
 
   return store.length<=0? <Spinner
@@ -38,9 +61,14 @@ export default function Products() {
       size='xl'
       />
     : <Box mt={'5%'}>
-    <Grid gridTemplateColumns={'10% 80%'} m={'auto'} gap={'10%'}>
+    <Grid gridTemplateColumns={'20% 75%'} gap={'1%'}>
         <VStack>
-          <Text>Filters</Text>
+        <Accordion allowToggle w={'80%'}>
+          <h3>FILTERS</h3>
+          {filterData.map((el)=>{
+            return filters(el.title,el.options);
+          })}
+        </Accordion>
         </VStack>
         <Grid gridTemplateColumns={{base:'repeat(1,98%)',sm:'repeat(2,45%)',md:'repeat(3,25%)'}} m={'auto'} gap={'1%'}>
           {store.map((el)=>{
