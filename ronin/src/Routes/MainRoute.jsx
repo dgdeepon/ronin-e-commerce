2f92a9451d2c974209202d64d3cdabe7e4c409a4
dgdeepon <deeponghosh890@gmail.com>
2023-04-16 16:47:32 +0530

    single product page demo

--- ronin/src/Routes/MainRoute.jsx
+++ ronin/src/Routes/MainRoute.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../components/Home/Home";
 import Products from "../components/Products";
+import SingleProduct from "../components/SingleProduct";
 
 export default function MainRoute() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products/>}/>
+      <Route path="/products/:id" element={<SingleProduct/>}/>
     </Routes>
   );
 }
